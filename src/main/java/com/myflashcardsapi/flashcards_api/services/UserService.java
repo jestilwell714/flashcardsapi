5dd715e8638499a3b0d831e2d0422dd9972e4ab6
jamesstilwell <jamesstilwelll@icloud.com>
2026-01-08 13:53:58 +1300

    Done implementation for UserService

--- src/main/java/com/myflashcardsapi/flashcards_api/services/UserService.java
+++ src/main/java/com/myflashcardsapi/flashcards_api/services/UserService.java
@@ -1,9 +1,12 @@
 package com.myflashcardsapi.flashcards_api.services;
 
 import com.myflashcardsapi.flashcards_api.domain.dto.UserDto;
+import org.apache.coyote.BadRequestException;
+
+import java.util.Optional;
 
 public interface UserService {
-    UserDto createUser(UserDto userDto);
-    UserDto getUserById(Long userId);
+    UserDto createUser(UserDto userDto) throws BadRequestException;
+    Optional<UserDto> getUserById(Long userId);
     boolean exists(Long userId);
 }
