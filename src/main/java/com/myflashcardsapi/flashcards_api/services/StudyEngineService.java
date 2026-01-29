b4b18241df2c0330d17f8f79814a8bd2e37f244b
jamesstilwell <jamesstilwelll@icloud.com>
2026-01-30 00:35:48 +1300

    Moved files around

--- src/main/java/com/myflashcardsapi/flashcards_api/services/StudyEngineService.java
+++
@@ -1,9 +0,0 @@
-package com.myflashcardsapi.flashcards_api.services;
-
-import com.myflashcardsapi.flashcards_api.domain.dto.FlashCardDto;
-
-import java.util.List;
-
-public interface StudyEngineService {
-    public List<FlashCardDto> getCardBatchForCramMode(List<FlashCardDto> flashCardDtoList);
-}
