40ea7aa164528e68944bc25bb08d1a91fa57ac28
jamesstilwell <jamesstilwelll@icloud.com>
2026-01-31 23:11:37 +1300

    removed setloading not nessacary and causing bug

--- flashcards-ui/src/components/FileExplorer.jsx
+++ flashcards-ui/src/components/FileExplorer.jsx
@@ -1,26 +1,26 @@
 import { useEffect, useParams, useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import CreateDeckOrFolder from "./CreateDeckOrFolder";
 
 
-export default function FileExplorer( onSelectFlashCard ) {
+export default function FileExplorer( {onSelectFlashCard} ) {
     const { type, id } = useParams();
     const [content,setContent] = useState(null);
-    const {isLoading, setLoading} = useState(false);
+    const [isCreate, setIsCreate] = useState(false);
+    const [createType, setCreateType] = useState(null);
+    const [showDropdown, setShowDropdown] = useState(false);
     
     const fetchContentsUrl = type === "root" ? '' : `${type}/${id}`;
 
 
     useEffect(() => {
-        setLoading(true);
-
         fetch(fetchContentsUrl)
         .then(response => response.json())
         .then(data => { 
                 setContent(data);
-                setLoading(false);
             }
         ).catch(err => console.error("Fetch failed:", err));
-    }, [fetchContentsUrl,setLoading]); 
+    }, [fetchContentsUrl]); 
 
     const navigate = useNavigate();
 
@@ -34,23 +34,43 @@ export default function FileExplorer( onSelectFlashCard ) {
         onSelectFlashCard(item, item.type, mode);
     }
 
+    function handleCreate(type ) {
+            setShowDropdown(false);
+            setIsCreate(true);
+            setCreateType(type);
+    }
+
+    function handleSubmit() {
+        setIsCreate(false);
+    }
+
     return (
-        isLoading ? ( 
-            <div>
-                <h1>Loading...</h1>
-            </div> 
-        ) : (
-            <>
+            <ul>
+                <li>
+                    <nav>
+                        <button onClick={() => setShowDropdown(!showDropdown)}>Create</button>
+                        {showDropdown && (
+                            <ul>
+                                <li onClick={() => handleCreate("folder")}>
+                                    <h4>Folder</h4>
+                                </li>
+                                <li onClick={() => handleCreate("deck")}>
+                                    <h4>Deck</h4>
+                                </li>
+                            </ul>
+                        )}
+                    </nav>
+                </li>
+                {isCreate ? <CreateDeckOrFolder type={createType} onSubmit={handleSubmit}/> : ''}
                 {content.map((item) => (
-                    <div key={item.id} onClick={() => handleClick(item)}>
+                    <li key={item.id} onClick={() => handleClick(item)}>
                         <span className="icon">
                                 {item.type === 'folder' ? '📁' : ''}
                                 {item.type === 'deck' ? '🎴' : ''}
                         </span>
                         <h3>{item.name}</h3>
-                    </div>
+                    </li>
                 ))}
-            </>
+            </ul>
         )
-    );
 }
